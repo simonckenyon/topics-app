52bb9244630ad244e686c1c7b82a0c331a61550d
Simon Kenyon <simon@horizonsp.co.uk>
2018-09-19 07:13:36 +0100

    removed unused swipe code. added code to signin to get a list of possible email addresses from the contact list.

---
+++ app/src/main/java/ie/koala/topics/feature/auth/TextInputAutoCompleteTextView.kt
@@ -0,0 +1,33 @@
+package ie.koala.topics.feature.auth
+
+import android.content.Context
+import android.support.design.widget.TextInputLayout
+import android.support.v7.widget.AppCompatAutoCompleteTextView
+import android.util.AttributeSet
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputConnection
+
+/**
+ * See https://stackoverflow.com/a/41864063
+ */
+class TextInputAutoCompleteTextView : AppCompatAutoCompleteTextView {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    override fun onCreateInputConnection(outAttrs: EditorInfo): InputConnection? {
+        val ic = super.onCreateInputConnection(outAttrs)
+        if (ic != null && outAttrs.hintText == null) {
+            // If we don't have a hint and our parent is a TextInputLayout, use it's hint for the
+            // EditorInfo. This allows us to display a hint in 'extract mode'.
+            val parent = parent
+            if (parent is TextInputLayout) {
+                outAttrs.hintText = parent.hint
+            }
+        }
+        return ic
+    }
+}
